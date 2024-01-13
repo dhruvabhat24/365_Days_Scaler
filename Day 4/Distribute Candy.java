f611c57313035c37c464ca30a867ddd8c9ec6052
Dhruva Bhat <dhruvabhat24@gmail.com>
2024-01-13 07:55:37 +0530

    Create Distribute Candy.java

---
+++ Day 4/Distribute Candy.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int candy(int[] A) {
+        int n = A.length;
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
+        for (int i = 1; i < n; i++) {
+            if (A[i] > A[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            if (A[i] > A[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+        int totalCandies = 0;
+        for (int candy : candies) {
+            totalCandies += candy;
+        }     
+        return totalCandies;
+    }
+}
